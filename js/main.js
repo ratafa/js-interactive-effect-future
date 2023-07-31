67c0eb7ce173c6e2b40d61b41472fd8baf7c65b2
youngchan Kim <aacz12035@gmail.com>
2023-07-31 16:28:07 +0900

    feat: startButton 설명 문구 추가 및 setTimeout 추가

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ const p = document.querySelector('.displayText');
 const scan = document.querySelector('.scan');
 const dummyBox = document.querySelector('.dummyBox');
 const startButton = document.querySelector('.startButton');
+const buttonDescription = document.querySelector('.buttonDescription');
 
 startButton.addEventListener('click', () => {
-  startButton.style.display = 'none';
-  handleScanHand();
-  setTimeout(() => {
-    scan.style.display = 'none';
-    dummyBox.style.visibility = 'visible';
-    handleTextDisplay(p);
-  }, 5300)
+  setTimeout( ()  => {
+    startButton.style.display = 'none';
+    buttonDescription.style.display = 'none';
+    handleScanHand();
+    setTimeout(() => {
+      scan.style.display = 'none';
+      dummyBox.style.visibility = 'visible';
+      handleTextDisplay(p);
+    }, 5300)
+  },2000)
 });
