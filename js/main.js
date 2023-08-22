7c0eb3a4b66606ea005345e1889a03ff67d29431
youngchan Kim <aacz12035@gmail.com>
2023-08-23 01:20:26 +0900

    feat: 모바일 접근 차단 기능 추가

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ import {handleScanHand} from "./scanHand.js";
 
 const p = document.querySelector('.displayText');
 const scan = document.querySelector('.scan');
+const mobileWarning = document.querySelector('.mobileWarning');
 const dummyBox = document.querySelector('.dummyBox');
 const startButton = document.querySelector('.startButton');
-const buttonDescription = document.querySelector('.buttonDescription');
+
+const isMobile = () => {
+  const user = navigator.userAgent;
+  if ( user.indexOf("iPhone") > -1 || user.indexOf("Android") > -1 ) {
+    startButton.style.display = 'none';
+    mobileWarning.style.display = 'block';
+  }
+}
+
+window.onload = () => {
+  isMobile();
+}
+
 
 startButton.addEventListener('click', () => {
     startButton.style.display = 'none';
