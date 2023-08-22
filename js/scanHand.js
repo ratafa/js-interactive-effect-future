f8d8b8444e6edc7658ed3964b38dea3924519230
youngchan Kim <aacz12035@gmail.com>
2023-08-23 00:58:32 +0900

    fix: 모바일 환경 애니메이션 불가로 인한 pc 화면 대응

--- js/scanHand.js
+++ js/scanHand.js
@@ -1,27 +1,11 @@
 const scan = document.querySelector('.scan');
-const hand2 = document.querySelector('.hand2');
-const lines = document.querySelector('.lines');
 const handScanText = document.querySelector('.handScanText');
-
-let yPoint = 0;
-let rafScan;
-const render = () => {
-  yPoint += 0.5;
-
-  hand2.style.height = `${yPoint}%`;
-  lines.style.top = `${yPoint}%`;
-
-  if(yPoint === 80) {
-    return cancelAnimationFrame();
-  }
-  rafScan = requestAnimationFrame(render)
-}
 
 export const handleScanHand = () => {
   scan.style.display = 'block';
-  requestAnimationFrame(render)
+
   setTimeout(() => {
     handScanText.innerText = 'Success!'
     handScanText.style.animation = 'none'
-  }, 3300)
+  }, 3700)
 }
