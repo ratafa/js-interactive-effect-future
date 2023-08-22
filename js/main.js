1fb46bd0836127c0b81d068382b90d51e33b9c6d
youngchan Kim <aacz12035@gmail.com>
2023-08-22 19:58:36 +0900

    fix: glitch button 클릭 수정

--- js/main.js
+++ js/main.js
@@ -8,14 +8,11 @@ const startButton = document.querySelector('.startButton');
 const buttonDescription = document.querySelector('.buttonDescription');
 
 startButton.addEventListener('click', () => {
-  setTimeout( ()  => {
     startButton.style.display = 'none';
-    buttonDescription.style.display = 'none';
     handleScanHand();
     setTimeout(() => {
       scan.style.display = 'none';
       dummyBox.style.visibility = 'visible';
       handleTextDisplay(p);
     }, 5300)
-  },2000)
 });
